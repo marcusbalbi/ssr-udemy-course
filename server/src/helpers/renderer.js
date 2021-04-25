bc09c6aaab82752b74fa3468f66073038815e88a
Marcus Balbi <balbimarcus@gmail.com>
2021-04-25 17:45:24 -0300

    staticrouter and browserrouter working

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
-import Home from "../client/components/Home";
+import { StaticRouter } from "react-router-dom";
+import Routes from "../client/Routes";
 
-const render = () => {
-  const content = renderToString(<Home />);
+const render = (req) => {
+  const content = renderToString(
+    <StaticRouter location={req.path} context={{}} >
+      <Routes />
+    </StaticRouter>
+  );
   return `
     <html>
       <head></head>
