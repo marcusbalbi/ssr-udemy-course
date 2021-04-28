f1dfcc1b4f686f1fb599dee491169ef319dcd745
Marcus Balbi <balbimarcus@gmail.com>
2021-04-28 18:13:37 -0300

    Header component created

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,16 +1,22 @@
+import App from "./App";
 import HomePage from "./pages/HomePage";
 import UsersListPage from "./pages/UsersListPage";
 
 const Routes = [
   {
-    ...HomePage,
-    path: "/",
-    exact: true,
-  },
-  {
-    ...UsersListPage,
-    path: "/users",
-    exact: true,
+    ...App,
+    routes: [
+      {
+        ...HomePage,
+        path: "/",
+        exact: true,
+      },
+      {
+        ...UsersListPage,
+        path: "/users",
+        exact: true,
+      },
+    ],
   },
 ];
 
