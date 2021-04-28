11121670912f79c1f7ba2eaf9e1f6b75b44987ea
Marcus Balbi <balbimarcus@gmail.com>
2021-04-28 20:51:31 -0300

    styling header

--- server/src/client/components/Header.js
+++ server/src/client/components/Header.js
@@ -3,21 +3,29 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
 const Header = ({ auth }) => {
-  
   const AuthButton = auth ? (
     <a href="/api/logout">Logout</a>
-  ) : <a href="/api/auth/google">Login</a>
-
+  ) : (
+    <a href="/api/auth/google">Login</a>
+  );
 
   return (
-    <div>
-      <Link to="/">React SSR</Link>
-      <div>
-        <Link to="/users">Users</Link>
-        <Link to="/admins">Admins</Link>
-        {AuthButton}
+    <nav>
+      <div className="nav-wrapper">
+        <Link to="/" className="brand-logo">
+          React SSR
+        </Link>
+        <ul className="right">
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+          <li>
+            <Link to="/admins">Admins</Link>
+          </li>
+          <li> {AuthButton}</li>
+        </ul>
       </div>
-    </div>
+    </nav>
   );
 };
 
