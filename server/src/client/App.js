b1bbcf550f1933aad53977d1f2b7420579d8ff66
Marcus Balbi <balbimarcus@gmail.com>
2021-04-28 18:23:23 -0300

    calling action inside App.js

--- server/src/client/App.js
+++ server/src/client/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { renderRoutes  } from "react-router-config"
 import Header from "./components/Header"
+import { fetchCurrentUser } from "./actions"
 const App = ({route}) => {
   return <div>
     <Header />
@@ -9,5 +10,8 @@ const App = ({route}) => {
 }
 
 export default {
-  component: App
+  component: App,
+  loadData: ({dispatch}) => {
+    return dispatch(fetchCurrentUser());
+  }
 };
