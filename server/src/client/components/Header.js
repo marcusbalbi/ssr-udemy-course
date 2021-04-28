011cdf69715d39995404b2bd043ab0f7e50faced
Marcus Balbi <balbimarcus@gmail.com>
2021-04-28 20:44:21 -0300

    add links

--- server/src/client/components/Header.js
+++ server/src/client/components/Header.js
@@ -3,10 +3,20 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
 const Header = ({ auth }) => {
-  console.log("My auth status is", auth)
+  
+  const AuthButton = auth ? (
+    <a href="/api/logout">Logout</a>
+  ) : <a href="/api/auth/google">Login</a>
+
+
   return (
     <div>
       <Link to="/">React SSR</Link>
+      <div>
+        <Link to="/users">Users</Link>
+        <Link to="/admins">Admins</Link>
+        {AuthButton}
+      </div>
     </div>
   );
 };
