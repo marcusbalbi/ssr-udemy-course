c96a54562cd0c5e9e504026ba9819d6c93861f27
Marcus Balbi <balbimarcus@gmail.com>
2021-04-28 20:15:00 -0300

    update react, react-redux redux

--- server/src/client/components/Header.js
+++ server/src/client/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 
 const Header = ({ auth }) => {
-  console.log("My auth status is", auth)
+  console.log("My auth status is", auth);
+  const hookAuth = useSelector((state) => state.auth);
+  console.log("hook auth is:", hookAuth);
   return (
     <div>
       <Link to="/">React SSR</Link>
