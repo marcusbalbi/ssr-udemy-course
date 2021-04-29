b354a50e9ed4754b47ba4f813828dab2c809b95b
Marcus Balbi <balbimarcus@gmail.com>
2021-04-29 20:59:03 -0300

    adminsPage

--- server/src/client/pages/AdminsListPage.js
+++ server/src/client/pages/AdminsListPage.js
@@ -1,40 +1,31 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { fetchAdmins } from "../actions";
 import requireAuth from "../components/hocs/requireAuth";
 
-class UsersList extends Component {
-  componentDidMount() {
-    this.props.fetchAdmins();
-  }
-
-  renderAdmins() {
-    return this.props.admins.map((admin) => {
+const AdminsList = () => {
+  const admins = useSelector((state) => state.admins);
+  useEffect(() => {
+    fetchAdmins();
+  }, []);
+  function renderAdmins() {
+    return admins.map((admin) => {
       return <li key={admin.id}>{admin.name}</li>;
     });
   }
-
-  render() {
-    return (
-      <div>
-        <h3>Lista de Usuários Admins</h3>
-        <ul>{this.renderAdmins()}</ul>
-      </div>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    admins: state.admins,
-  };
-}
+  return (
+    <div>
+      <h3>Lista de Usuários Admins</h3>
+      <ul>{renderAdmins()}</ul>
+    </div>
+  );
+};
 
 function loadData(store) {
   return store.dispatch(fetchAdmins());
 }
 
 export default {
-  component: connect(mapStateToProps, { fetchAdmins })(requireAuth(UsersList)),
+  component: requireAuth(AdminsList),
   loadData,
 };
