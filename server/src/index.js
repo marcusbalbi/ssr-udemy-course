9ef857cb117fe49d678a06c165ab2f30fa9925fd
Marcus Balbi <balbimarcus@gmail.com>
2021-04-28 22:07:15 -0300

    error handling admins page

--- server/src/index.js
+++ server/src/index.js
@@ -23,16 +23,24 @@ app.use(express.static("public"));
 app.get("*", (req, res) => {
   const store = createStore(req);
 
-  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
-    return route.loadData ? route.loadData(store) : null;
-  });
+  const promises = matchRoutes(Routes, req.path)
+    .map(({ route }) => {
+      return route.loadData ? route.loadData(store) : null;
+    })
+    .map((promise) => {
+      if (promise) {
+        return new Promise((resolve, reject) => {
+          return promise.then(resolve).catch(resolve);
+        });
+      }
+    });
 
   Promise.all(promises).finally(() => {
-    const context = {}
+    const context = {};
     const content = renderer(req, store, context);
 
     if (context.notFound) {
-      res.status(404)
+      res.status(404);
     }
 
     res.send(content);
